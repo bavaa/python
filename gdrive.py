166ea3e9b9f41c3809a1603ecbc7f5eb7b954981
bava <41218759+bavaa@users.noreply.github.com>
2019-06-26 06:21:47 +0000

    Update gdrive.py

--- gdrive.py
+++ gdrive.py
@@ -2,7 +2,7 @@ import os
 def gdrive():
     os.popen("cd ~")
     os.popen("wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download")
-    if (not os.path.exists("uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE"))
+    if (not os.path.exists("uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE")):
         os.popen("sudo apt install wget")
         os.popen("wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download")
     os.popen("mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive")
