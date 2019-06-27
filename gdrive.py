3ff752e32da1ec817011b94d09c228430978247a
bava <41218759+bavaa@users.noreply.github.com>
2019-06-27 07:50:51 +0000

    Update gdrive.py

--- gdrive.py
+++ gdrive.py
@@ -1,16 +1,16 @@
 import os
 def gdrive():
     os.popen("cd ~")
-    os.popen("rm -rf uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE")
-    if (not os.path.exists("uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE")):
-        os.popen("sudo apt install wget")
-        os.popen("wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download")
-    os.popen("mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive")
+    os.popen("aria2c https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download")
+    if (not os.path.exists("gdrive-linux-x64")):
+        os.popen("sudo apt install -y aria2")
+        os.popen("aria2c https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download")
+    os.popen("mv gdrive-linux-x64 gdrive")
     os.popen("chmod +x gdrive")
     os.popen("sudo install gdrive /usr/local/bin/gdrive")
-        activation_link = os.popen("gdrive list")
-    file = open("abc.txt","w")
+    os.popen("gdrive list")
+    '''file = open("abc.txt","w")
     file.write(activation_link)
-    file.close()
+    file.close()'''
 
 gdrive()
