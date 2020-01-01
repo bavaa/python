e87b07ad8bb276f704cb69523edc64a6c20fbb38
bava <41218759+bavaa@users.noreply.github.com>
2020-01-01 10:20:12 +0530

    Update try 2 downloader.py

--- try 2 downloader.py
+++ try 2 downloader.py
@@ -1,25 +1,21 @@
-from subprocess import Popen,PIPE
+from subprocess import Popen,PIPE,STDOUT
 def torrent_link (link):
     process=Popen(["aria2c", "--seed-time=0", link],stdout=PIPE,stderr=PIPE)
     if process.stderr.read() is not None:
         print(process.stdout.read())
     else:
         print(process.stdout.read())
-
 def url_link (link):
+    from subprocess import Popen,PIPE,STDOUT
     #process=Popen(["aria2c", "-x4", "-k1M", link],stdout=PIPE,stderr=PIPE)
-    process=Popen(["wget",link],stdout=PIPE,stderr=PIPE)
-    process_out=process.stdout.read()
-    proecss_err=process.stderr.read()
-    if process.stderr.read() is not None:
-        print("error")
-        print(proecss_err)
-    else:
-        print("output")
-        print(process_out)
-
+    process=Popen(["wget",link],stdout=PIPE,stderr=STDOUT)
+    while True:
+        output = process.stdout.readline()
+        if output == b"" and process.poll() is not None:
+            return
+        if output:
+            print(output.decode("utf-8").strip())
 link=str(input("enter the link"))
-print(link)
 if link.startswith("magnet:?"):
     torrent_link(link)
     
